4b7bacacfe9aacfd24ff7ccf907766a59ca4413b
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-04-09 13:23:40 +0200

    Add endpoint to retrieve a single patient

--- lib/vandelay/rest/patients_patient.rb
+++ lib/vandelay/rest/patients_patient.rb
@@ -4,8 +4,20 @@ require 'vandelay/services/patient_records'
 module Vandelay
   module REST
     module PatientsPatient
+      def self.patients_srvc
+        @patients_srvc ||= Vandelay::Services::Patients.new
+      end
+
       def self.registered(app)
-        # add endpoint code here
+        app.get '/patients/:id' do
+          results = Vandelay::REST::PatientsPatient.patients_srvc.retrieve_one(params['id'])
+          json([results])
+        end
+
+        app.error do
+          e = env['sinatra.error']
+          json message: e.message
+        end
       end
     end
   end
